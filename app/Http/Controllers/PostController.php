4fc9ef1be6fdfbb37d171a78ab3f8fc824a6fda2
Gabriel Gagno <gjpgagno@gmail.com>
2019-01-10 22:07:54 +0800

    Added associations for Post and User

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        
+    }
+
+    public function findByUser($id)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update($id, Request $request)
+    {
+
+    }
+
+    public function destroy($id, Request $request)
+    {
+
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body',
+        'tags',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
