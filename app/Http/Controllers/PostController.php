fd960cde48376e0f543731e340588ef0e1537a44
Gabriel Gagno <gjpgagno@gmail.com>
2019-01-12 00:04:17 +0800

    Added put and delete APIs

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,13 +38,34 @@ class PostController extends Controller
         return response()->json($this->responseBuilder->resSuccess($post->toArray()));
     }
 
-    public function update(Post $post, Request $request)
+    public function update(Request $request, Post $post)
     {
+        $input = $request->all();
+        // TODO validate here
 
+        DB::beginTransaction();
+        try {
+            $post->update($input);
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json($this->responseBuilder->resError('Error in saving resource', 500, '00'));
+        }
+
+        return response()->json($this->responseBuilder->resSuccess($post->toArray()));
     }
 
-    public function destroy($id, Request $request)
+    public function destroy(Request $request, Post $post)
     {
+        DB::beginTransaction();
+        try {
+            $post->delete();
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json($this->responseBuilder->resError('Error in deleting resource', 500, '00'));
+        }
 
+        return response()->json($this->responseBuilder->resSuccess(['message' => 'resource successfully deleted']));
     }
 }
