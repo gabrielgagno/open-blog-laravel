c14321bb8071adfc7386d564633e280fea4d999f
Gabriel John Gagno <gjpgagno@gmail.com>
2019-01-17 01:52:54 +0800

    Added StoreUpdateUsers form request Added special rule for Role Ids Added user controllers validations

---
+++ app/Rules/RoleIdExists.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use App\Role;
+
+class RoleIdExists implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $role = Role::where('id', $value)->first();
+
+        if($role) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must be an existing :attribute.';
+    }
+}
