e3920f509fafa57c7126796097996baa6f169100
Gabriel John Gagno <gjpgagno@gmail.com>
2019-01-18 22:40:48 +0800

    Removed example tests added new viewposts test

---
+++ tests/Feature/ViewPostsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ViewPostsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUserGet()
+    {
+        $response = $this->json('GET', '/posts');
+
+        $response->assertStatus(200);
+    }
+}
